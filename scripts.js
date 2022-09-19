26bbf6e77428bb7c6a3ab27e9d80ac02ea264240
Theophilus Adesola <theophilusadessola@gmail.com>
2022-09-19 11:04:44 +0100

    scripts.js has been added and can check if the number is even, divisible by 3 and 5

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function checkNum() {
+    var userNum = interFace.value;
+    if (userNum == 0) {
+        interFace.value = "Math Error!";
+    } else if (userNum % 2 == userNum % 3 && userNum % 2 == userNum % 5) {
+        interFace.value = userNum + " is definitely an even number";
+    } else if (userNum % 2 == 0) {
+        interFace.value = userNum + " is an even Number!!";
+    } else if (userNum % 3 == 0) {
+        interFace.value = userNum + "  is divisible by 3.";
+    } else if (userNum % 5 == 0) {
+        interFace.value = userNum + " is divisible by 5. ";
+    } else {
+        interFace.value = userNum + " is an odd number!!";
+    }
+    // else if((userNum == "")) {
+    //     interFace.value = "Enter something valid"
+    // }
+}
